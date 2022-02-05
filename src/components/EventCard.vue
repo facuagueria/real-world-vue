a604953b71480b26880822d99eccd55e04d1e12d
facundoagueria1@gmail.com <facundoagueria1@gmail.com>
2022-02-04 21:35:37 -0300

    First commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="event-card">
+    <span>@ {{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  data() {
+    return {};
+  },
+  props: {
+    event: Object,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="events">
+    <h1>Events for Good</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 11111,
+          category: "animal welfare",
+          title: "Cat Adoption Day",
+          description: "Find your new feline friend at this event.",
+          location: "Meow Town",
+          date: "January 28, 2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Kat Laydee",
+        },
+        {
+          id: 22222,
+          category: "animal welfare2",
+          title: "Cat Adoption Day2",
+          description: "Find your new feline friend at this event2.",
+          location: "Meow Town2",
+          date: "January 28, 20222",
+          time: "12:002",
+          petsAllowed: true,
+          organizer: "Kat Laydee2",
+        },
+        {
+          id: 33333,
+          category: "animal welfare3",
+          title: "Cat Adoption Day3",
+          description: "Find your new feline friend at this event.3",
+          location: "Meow Town3",
+          date: "January 28, 20223",
+          time: "12:003",
+          petsAllowed: true,
+          organizer: "Kat Laydee3",
+        },
+      ],
+    };
   },
 };
 </script>
+<style>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
